5e5ff9d67d9e4989a4ac426f66efa0c18ff3c80f
john <chukwuchidieberejohn@gmail.com>
2023-01-07 23:30:19 +0100

    initial commit

--- ConsoleMusicPlayer/ConsoleMusicPlayer/App/Controller.cs
+++ ConsoleMusicPlayer/ConsoleMusicPlayer/App/Controller.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using ConsoleMusicPlayer.Domain.Entities;
 using ConsoleMusicPlayer.Domain.Enums;
 using ConsoleMusicPlayer.Domain.Interfaces;
